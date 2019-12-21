589dedfc4bed8e62b4266b1a619ecf13a4bd3f8c
Arnav Gupta <arnav@codingblocks.com>
2019-12-22 04:50:20 +0530

    app: [ui] make place for avatar in repo cards
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/src/main/java/tech/arnav/githubtrending/ui/adapters/RepositoryListAdapter.kt
+++ app/src/main/java/tech/arnav/githubtrending/ui/adapters/RepositoryListAdapter.kt
@@ -1,7 +1,15 @@
 package tech.arnav.githubtrending.ui.adapters
 
+import android.graphics.Color
+import android.os.Build
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.BulletSpan
+import android.text.style.TextAppearanceSpan
 import android.view.View
 import kotlinx.android.synthetic.main.list_item_repository.view.*
+import tech.arnav.githubtrending.R
 import tech.arnav.lib.trendinggithub.models.Repository
 
 
@@ -12,6 +20,30 @@ class RepositoryListAdapter(
     BaseListAdapter<Repository>(layoutId, bindView) {
 
     override fun baseBindView(itemView: View, item: Repository) {
-        itemView.tvRepoName.text = item.name
+        val repoName = SpannableString("${item.author} / ${item.name}")
+        repoName.setSpan(
+            TextAppearanceSpan(itemView.context, R.style.TextAppearance_MaterialComponents_Headline6),
+            repoName.indexOfFirst { it == '/' },
+            repoName.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        itemView.tvRepoName.text = repoName
+
+        item.language?.let {
+            val lang = SpannableString(it)
+
+            val langBullet = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                BulletSpan(4, Color.parseColor(item.languageColor), 10)
+            } else {
+                BulletSpan(4, Color.parseColor(item.languageColor))
+            }
+
+            lang.setSpan(
+                langBullet,
+                0 , 0,
+                Spannable.SPAN_MARK_MARK
+            )
+            itemView.tvLanguage.text = lang
+        }
     }
 }
